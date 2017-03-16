f4499992929bce7863faec3a1c2c8b833200e096
Renaat De Muynck <renaat.demuynck@gmail.com>
2017-03-16 14:55:48 +0100

    Create database user for application

--- bin/install.sh
+++ bin/install.sh
@@ -4,5 +4,9 @@
 mkdir -p /vagrant/data/logs
 
 # Create database 'zf3-skeleton-application'
-mysql -u root -e 'CREATE DATABASE `zf3-skeleton-application`'
+mysql -u root <<-"SQL"
+	CREATE DATABASE `zf3-skeleton-application`;
+	CREATE USER `zf3-skeleton-application`@`%` IDENTIFIED BY "zf3-skeleton-application";
+	GRANT ALL PRIVILEGES ON *.* TO `zf3-skeleton-application`@`%`;
+SQL
 /vagrant/vendor/bin/doctrine-module orm:schema-tool:create
